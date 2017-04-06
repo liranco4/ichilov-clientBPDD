ff3ecfb4d5b6fb82b51bf59eaebc14d0220f1862
Liran Cohen <liran.cohen@vatbox.com>
2017-04-06 19:59:19 +0300

    first commit

---
+++ Ichilov/app/src/main/java/com/example/liran/ichilov/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.liran.ichilov;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
